26d7e7ce59a09e2a4bcdb7f5d639081ffadc5720
Yousef497 <yousef.shalaby70@gmail.com>
2023-03-20 13:15:40 +0200

    Add files via upload

---
+++ Library-Management-System-code/GUI/imagewidget.cpp
@@ -0,0 +1,23 @@
+#include "imagewidget.h"
+
+imageWidget::imageWidget(string name , string imagePath) : QWidget()
+{
+    this->layout = new QVBoxLayout();
+    this->label = new QLabel;
+    this->Name = new QLabel(QString::fromStdString(name));
+
+        this->pixMap = new QPixmap(QCoreApplication::applicationDirPath() + "/../../Library-Management-System/icons/book1.png");
+    this->label->setPixmap(pixMap->scaled(this->width(),this->height(),Qt::KeepAspectRatio) );
+    this->label->setScaledContents(true);
+
+
+    this->layout->addWidget(label);
+    this->layout->addWidget(Name,0,Qt::AlignCenter);
+    this->setLayout(this->layout);
+}
+
+void imageWidget::mousePressEvent(QMouseEvent *event)
+{
+    cout  << this->Name->text().toStdString() << " widget is clicked " << endl;
+    emit bookClicked(this->Name->text().toStdString());
+}
